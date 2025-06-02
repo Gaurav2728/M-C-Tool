d6808bbc548f064ceef4fda1e2ae6a91ec6b3c4d
Amit Natani <amit.natani@gluedata.com>
2025-06-02 20:32:17 +0530

    Setup Preprod environment and here we have production keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, "git@github.com:GlueData-Services/M-C-Tool.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask(:password, nil, echo: false)
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
@@ -19,6 +20,8 @@ set :repo_url, "git@github.com:GlueData-Services/M-C-Tool.git"
 
 # Default value for :pty is false
 set :pty, true
+
+set :puma_bind,       "tcp://0.0.0.0:3000"
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml", 'config/master.key'
--- config/puma-prod.rb
+++ config/puma-prod.rb
@@ -12,10 +12,10 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+#bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
 pidfile "#{shared_dir}/tmp/pids/puma.pid"
